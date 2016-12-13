98b9b470a7ff79d340de5cb0743c339102086f2b
masonposch <masonposch@Mason-Poschs-MacBook-Pro.local>
2016-12-12 23:04:41 -0600

    server.js setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
+var logger = require("morgan");
 var mongoose = require("mongoose");
 
 
@@ -18,3 +19,70 @@ mongoose.Promise = Promise;
 
 // Initialize Express
 var app = express();
+
+
+
+
+
+//ESSENTIALS
+//=============
+
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+//Make public a static directory
+app.use(static.express('public'));
+
+//Database configuration with Mongoose
+mongoose.connect("LOCAL PATH TO MONGO SCHEMA");
+var db = mongoose.connection;
+
+//Show any mongoose errors
+db.on('error', function(error) {
+	console.log("Mongoose error" + error);
+});
+
+//On successful connection
+db.once('open', function(req, res) {
+	console.log("Mongoose connection successful");
+});
+
+
+
+
+
+//ROUTES
+//===========
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
+
+
+
+
+
+
+
+
+
+
